43c80d6e94f30e229b9cdf0920737f6e10eb5578
Iron Man <lohekabanda@gmail.com>
2025-11-12 18:53:53 +0000

    1
    Install our package
    
    Start by installing @vercel/analytics in your ex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: 'shivamghaware',
@@ -24,6 +25,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
